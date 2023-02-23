c851bbf90c9502caf0badfc7c1fc9cb261fc607f
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-02-23 22:51:30 +0800

    Add missing figures

--- experiments/copy_profiles.py
+++ experiments/copy_profiles.py
@@ -8,6 +8,8 @@ def copy_all_figures():
     copyfile("archives/perf/plain/1-50/perf-bobyqa_cobyla_lincoa_newuoa-unconstrained.pdf", "../figures/perf-plain-pdfo-50.pdf")
     copyfile("archives/perf/nan/1-50/rerun-1_rate-0.01/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-1-0.01.pdf")
     copyfile("archives/perf/nan/1-50/rerun-10_rate-0.01/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-10-0.01.pdf")
+    copyfile("archives/perf/nan/1-50/rerun-1_rate-0.05/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-1-0.05.pdf")
+    copyfile("archives/perf/nan/1-50/rerun-10_rate-0.05/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-10-0.05.pdf")
     copyfile("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-06/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-noisy-bfgs_cg_pdfo-50-6.pdf")
     copyfile("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-08/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-noisy-bfgs_cg_pdfo-50-8.pdf")
     copyfile("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-10/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-noisy-bfgs_cg_pdfo-50-10.pdf")
