4ad74be6b3ce393f74b4e8ea05df01ac5b10afb8
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-03-30 15:06:52 +0800

    Do not load history

--- experiments/run_profiles.py
+++ experiments/run_profiles.py
@@ -3,7 +3,7 @@ import os
 from copy_profiles import copy_all_figures
 from profiles import Profiles, set_loglevel
 
-COPY_PAPER_FIGURES = False
+COPY_PAPER_FIGURES = True
 
 
 if __name__ == "__main__":
@@ -12,23 +12,17 @@ if __name__ == "__main__":
 
     # Generate the performance and data profiles on the plain problems with n <= 50.
     profiles = Profiles(1, 50, "unconstrained")
-    profiles(["CG", "BFGS", "NEWUOA"], ["CG", "BFGS", "PDFO"])
-    del profiles
+    profiles(["CG", "BFGS", "NEWUOA"], ["CG", "BFGS", "PDFO"], load=False)
 
     # Generate the performance and data profiles on the noisy problems with n <= 50 and different noise levels.
     for noise_level in [1e-10, 1e-8]:
         profiles = Profiles(1, 50, "unconstrained", feature="noisy", noise_level=noise_level)
-        profiles(["CG", "BFGS", "NEWUOA"], ["CG", "BFGS", "PDFO"])
-        del profiles
+        profiles(["CG", "BFGS", "NEWUOA"], ["CG", "BFGS", "PDFO"], load=False)
 
     # Generate the performance and data profiles on the problems containing NaNs.
-    profiles = Profiles(1, 50, "unconstrained", feature="nan", nan_rate=0.01)
-    profiles(["CG", "BFGS", "PDFO", "PDFO-(no-barrier)"])
-    del profiles
-
-    profiles = Profiles(1, 50, "unconstrained", feature="nan", nan_rate=0.05)
-    profiles(["CG", "BFGS", "PDFO", "PDFO-(no-barrier)"])
-    del profiles
+    for nan_rate in [0.01, 0.05]:
+        profiles = Profiles(1, 50, "unconstrained", feature="nan", nan_rate=nan_rate)
+        profiles(["CG", "BFGS", "PDFO", "PDFO-(no-barrier)"], load=False)
 
     # Generate the profiles for the paper.
     os.chdir(cwd)
