0990302541f6a3fa6ca83f880fa44d55cf39879e
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-03-30 14:19:48 +0800

    Rename PDF files

--- experiments/copy_profiles.py
+++ experiments/copy_profiles.py
@@ -1,16 +1,24 @@
 import os
-from shutil import copyfile
+
+from PyPDF2 import PdfReader, PdfWriter
+
+
+def extract_pdf_page(input_pdf, page_number, output_pdf):
+    pdf_reader = PdfReader(input_pdf)
+    pdf_writer = PdfWriter()
+    pdf_writer.add_page(pdf_reader.pages[page_number - 1])
+    with open(output_pdf, "wb") as f:
+        pdf_writer.write(f)
+
 
 def copy_all_figures():
     os.makedirs("../figures", exist_ok=True)
-    copyfile("archives/perf/plain/1-50/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-plain-bfgs_cg_pdfo-50.pdf")
-    copyfile("archives/perf/nan/1-50/rerun-1_rate-0.01/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-1-0.01.pdf")
-    copyfile("archives/perf/nan/1-50/rerun-10_rate-0.01/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-10-0.01.pdf")
-    copyfile("archives/perf/nan/1-50/rerun-1_rate-0.05/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-1-0.05.pdf")
-    copyfile("archives/perf/nan/1-50/rerun-10_rate-0.05/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", "../figures/perf-nan-bfgs_cg_pdfo-50-10-0.05.pdf")
-    copyfile("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-06/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-noisy-bfgs_cg_pdfo-50-6.pdf")
-    copyfile("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-08/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-noisy-bfgs_cg_pdfo-50-8.pdf")
-    copyfile("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-10/perf-bfgs_cg_newuoa-unconstrained.pdf", "../figures/perf-noisy-bfgs_cg_pdfo-50-10.pdf")
+    for page in [2, 4]:
+        extract_pdf_page("archives/perf/plain/1-50/perf-bfgs_cg_newuoa-unconstrained.pdf", page, f"../figures/perf-plain-bfgs_cg_pdfo-50-{page}.pdf")
+        extract_pdf_page("archives/perf/nan/1-50/rerun-10_rate-0.01/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", page, f"../figures/perf-nan-bfgs_cg_pdfo-50-0.01-{page}.pdf")
+        extract_pdf_page("archives/perf/nan/1-50/rerun-10_rate-0.05/perf-bfgs_cg_pdfo_pdfo-(no-barrier)-unconstrained.pdf", page, f"../figures/perf-nan-bfgs_cg_pdfo-50-0.05-{page}.pdf")
+        extract_pdf_page("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-08/perf-bfgs_cg_newuoa-unconstrained.pdf", page, f"../figures/perf-noisy-bfgs_cg_pdfo-50-8-{page}.pdf")
+        extract_pdf_page("archives/perf/noisy/1-50/rerun-10_type-relative_level-1e-10/perf-bfgs_cg_newuoa-unconstrained.pdf", page, f"../figures/perf-noisy-bfgs_cg_pdfo-50-10-{page}.pdf")
 
 
 if __name__ == "__main__":
